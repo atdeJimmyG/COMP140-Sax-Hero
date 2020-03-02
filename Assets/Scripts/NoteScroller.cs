252a31373edd44378b11d7e3d59965d39fc434cd
atdeJimmyG <jamesgilljames9@gmail.com>
2020-03-02 16:30:58 +0000

    Added moving sideways arrows

--- Assets/Scripts/NoteScroller.cs
+++ Assets/Scripts/NoteScroller.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class NoteScroller : MonoBehaviour
 {
+
+    public float beatTempo;
+
+    public bool hasStarted;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        beatTempo = beatTempo / 60f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasStarted) {
+            if (Input.anyKeyDown) {
+                hasStarted = true;
+            }
+
+        }
+        else {
+            transform.position -= new Vector3(beatTempo * Time.deltaTime, 0f, 0f);
+        }
     }
 }
+
