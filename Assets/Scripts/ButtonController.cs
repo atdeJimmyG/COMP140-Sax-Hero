da4de908e22402d98a3a734de11d70ceeba0cc2f
atdeJimmyG <jamesgilljames9@gmail.com>
2020-03-02 16:07:53 +0000

    Added base project woth button controller

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite defultImage;
+    public Sprite pressedImage;
+
+    public KeyCode KeyToPress;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyToPress)) {
+            spriteRenderer.sprite = pressedImage;
+        }
+
+        if (Input.GetKeyUp(KeyToPress)) {
+            spriteRenderer.sprite = defultImage;
+        }
+    }
+}
